5836d1cebea5f3db0ed4346e18a0a28e4a8d1c96
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-02-08 17:54:41 +0100

    deprecated unit, use Olf.RTL.Language instead

--- u_langue.pas
+++ u_langue.pas
@@ -1,57 +1,25 @@
 unit u_langue;
 
 // (c) Patrick Prémartin / Olf Software 08/2016
+//
+// This file is distributed under AGPL license.
+//
+// Get the latest version at
+// https://github.com/DeveloppeurPascal/librairies
 
 interface
+
+uses Olf.RTL.Language;
 
 function GetOSLangID: String;
 
 implementation
 
-uses FMX.Platform
-{$IFDEF MACOS}
-,iOSAPI.Foundation, MacAPI.ObjectiveC
-{$ENDIF}
-;
-
-// récupération du code langue de l'appareil
-// https://forums.embarcadero.com/thread.jspa?threadID=108333
-// ou http://www.synaptica.info/en/2015/12/21/delphi-10seattle-get-current-device-language/
-// ou  http://codeverge.com/embarcadero.delphi.firemonkey/detect-current-language-on-andr/2001235#sthash.zjLIi2KY.dpuf
+{$MESSAGE WARN 'u_langue unit is deprecated. Use Olf.RTL.Language unit instead.'}
 
 function GetOSLangID: String;
-{$IFDEF MACOS}
-var
-  Languages: NSArray;
 begin
-  Languages := TNSLocale.OCClass.preferredLanguages;
-  Result := TNSString.Wrap(Languages.objectAtIndex(0)).UTF8String;
-{$ENDIF}
-{$IFDEF ANDROID}
-  var
-    LocServ: IFMXLocaleService;
-  begin
-    if TPlatformServices.Current.SupportsPlatformService(IFMXLocaleService,
-      IInterface(LocServ)) then
-      Result := LocServ.GetCurrentLangID;
-{$ENDIF}
-{$IFDEF MSWINDOWS}
-    var
-      buffer: MarshaledString;
-      UserLCID: LCID;
-      BufLen: Integer;
-    begin
-      // defaults
-      UserLCID := GetUserDefaultLCID;
-      BufLen := GetLocaleInfo(UserLCID, LOCALE_SISO639LANGNAME, nil, 0);
-      buffer := StrAlloc(BufLen);
-      if GetLocaleInfo(UserLCID, LOCALE_SISO639LANGNAME, buffer, BufLen) <> 0
-      then
-        Result := buffer
-      else
-        Result := 'en';
-      StrDispose(buffer);
-{$ENDIF}
-    end;
+  result := GetCurrentLanguageISOCode;
+end;
 
 end.
