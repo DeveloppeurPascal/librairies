d107ab3bf2dc7851eea33791294c0f5856995d10
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-03-12 17:24:17 +0100

    updated the Olf.RTL.CryptDecrypt sample to use an instance and the class

--- samples/RTL.CryptDecrypt/SimpleCryptDecryptSample/fMain.pas
+++ samples/RTL.CryptDecrypt/SimpleCryptDecryptSample/fMain.pas
@@ -13,8 +13,10 @@ type
     Edit1: TEdit;
     Button1: TButton;
     Memo1: TMemo;
+    Button2: TButton;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -96,6 +98,67 @@ begin
   finally
     crypt.free;
   end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  s: string;
+  ss: TStringStream;
+  cs, dcs: TStream;
+  nb, i: integer;
+  key: TByteDynArray;
+  KeyToString: string;
+begin
+  s := Edit1.text;
+  if s.isempty then
+    raise exception.create('Please give me a string to crypt/decrypt.');
+
+  // Init the private key
+  KeyToString := 'Key :';
+  nb := random(length(s) + 1);
+  setlength(key, nb);
+  for i := 0 to nb - 1 do
+  begin
+    key[i] := random(255);
+    if (i = 0) then
+      while (key[0] in [0, 255]) do
+        key[0] := random(255);
+    KeyToString := KeyToString + ' ' + key[i].tostring;
+  end;
+
+  Memo1.lines.add(s);
+  Memo1.lines.add('s.length=' + s.length.tostring);
+
+  // Crypt the string
+  ss := TStringStream.create(s);
+  try
+    Memo1.lines.add('ss.size=' + ss.size.tostring);
+    ShowStreamContent('ss', ss);
+    cs := TOlfCryptDecrypt.crypt(ss, key);
+    Memo1.lines.add('cs.size=' + cs.size.tostring);
+    ShowStreamContent('cs', cs);
+  finally
+    ss.free;
+  end;
+
+  // Uncrypt the string
+  dcs := TOlfCryptDecrypt.Decrypt(cs, key);
+  Memo1.lines.add('dcs.size=' + dcs.size.tostring);
+  ShowStreamContent('dcs', dcs);
+  cs.free;
+  ss := TStringStream.create;
+  try
+    dcs.position := 0;
+    ss.CopyFrom(dcs);
+    ShowStreamContent('ss', ss);
+    Memo1.lines.add('ss.size=' + ss.size.tostring);
+    dcs.free;
+    s := ss.DataString;
+    Memo1.lines.add('s.length=' + s.length.tostring);
+  finally
+    ss.free;
+  end;
+  Memo1.lines.add(s);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
