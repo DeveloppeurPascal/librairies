4784d3b4aff530c0b587553c223d78c5d3945f53
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-04-18 14:38:34 +0200

    added XMLDoc comments to unit Olf.RTL.CryptDecrypt (#67)

--- src/Olf.RTL.CryptDecrypt.pas
+++ src/Olf.RTL.CryptDecrypt.pas
@@ -2,53 +2,113 @@ unit Olf.RTL.CryptDecrypt;
 
 interface
 
-// This unit contains very simple cryptographic algorithms.
-// DON't USE THEM FOR SENSIBLE DATAS !!!
-//
-// Check those projects if you need a more robust algorithm :
-//
-// - Delphi Encryption Compendium
-// https://github.com/MHumm/DelphiEncryptionCompendium
-//
-// - TMS Cryptographic Pack
-// https://www.tmssoftware.com/site/tmscrypto.asp
-
 uses
   System.Classes,
   System.Types;
 
 type
+  /// <summary>
+  /// Simple class to crypt and decrypt buffers.
+  /// </summary>
+  /// <remarks>
+  /// This unit contains very simple cryptographic algorithms.
+  /// DON't USE THEM FOR SENSIBLE DATAS !!!
+  ///
+  /// Check those projects if you need a more robust algorithm :
+  ///
+  /// - Delphi Encryption Compendium
+  /// https://github.com/MHumm/DelphiEncryptionCompendium
+  ///
+  /// - TMS Cryptographic Pack
+  /// https://www.tmssoftware.com/site/tmscrypto.asp
+  ///
+  /// </remarks>
   TOlfCryptDecrypt = class
   private
     FKeys: TByteDynArray;
     procedure SetKeys(const Value: TByteDynArray);
   protected
   public
+    /// <summary>
+    /// buffer of bytes used as a key by Crypt/Decrypt functions
+    /// </summary>
     property Keys: TByteDynArray read FKeys write SetKeys;
 
+    /// <summary>
+    /// use XOR operand to crypt a buffer with the keys buffer property
+    /// </summary>
     function XORCrypt(Const AStream: TStream): TStream; overload;
+
+    /// <summary>
+    /// use XOR operand to crypt a buffer with a keys buffer
+    /// </summary>
     class function XORCrypt(Const AStream: TStream; Const AKeys: TByteDynArray)
       : TStream; overload;
+
+    /// <summary>
+    /// use XOR operand to decrypt a buffer with the keys buffer property
+    /// </summary>
     function XORDecrypt(Const AStream: TStream): TStream; overload;
+
+    /// <summary>
+    /// use XOR operand to decrypt a buffer with a keys buffer
+    /// </summary>
     class function XORDecrypt(Const AStream: TStream;
       Const AKeys: TByteDynArray): TStream; overload;
 
+    /// <summary>
+    /// exchange bytes between a buffer to crypt and the property key
+    /// </summary>
     function SwapCrypt(Const AStream: TStream): TStream; overload;
+
+    /// <summary>
+    /// exchange bytes between a buffer to crypt and the key parameter
+    /// </summary>
     class function SwapCrypt(Const AStream: TStream; Const AKeys: TByteDynArray)
       : TStream; overload;
+
+    /// <summary>
+    /// exchange bytes between a buffer to uncrypt and the property key
+    /// </summary>
     function SwapDecrypt(Const AStream: TStream): TStream; overload;
+
+    /// <summary>
+    /// exchange bytes between a buffer to uncrypt and the key parameter
+    /// </summary>
     class function SwapDecrypt(Const AStream: TStream;
       Const AKeys: TByteDynArray): TStream; overload;
 
+    /// <summary>
+    /// Create an instance of TOlfCryptDecrypt class
+    /// </summary>
     constructor Create; overload;
+
+    /// <summary>
+    /// Create an instance of TOlfCryptDecrypt class and fill its key buffer
+    /// </summary>
     constructor Create(Const AKeys: TByteDynArray); overload;
 
+    /// <summary>
+    /// DEPRECATED - internally use XORCrypt
+    /// </summary>
     function Crypt(Const AStream: TStream): TStream; overload;
       deprecated 'Use XORCrypt()';
+
+    /// <summary>
+    /// DEPRECATED - internally use XORCrypt
+    /// </summary>
     class function Crypt(Const AStream: TStream; Const AKeys: TByteDynArray)
       : TStream; overload; deprecated 'Use XORCrypt()';
+
+    /// <summary>
+    /// DEPRECATED - internally use XORDecrypt
+    /// </summary>
     function Decrypt(Const AStream: TStream): TStream; overload;
       deprecated 'Use XORDecrypt()';
+
+    /// <summary>
+    /// DEPRECATED - internally use XORDecrypt
+    /// </summary>
     class function Decrypt(Const AStream: TStream; Const AKeys: TByteDynArray)
       : TStream; overload; deprecated 'Use XORDecrypt()';
   end;
