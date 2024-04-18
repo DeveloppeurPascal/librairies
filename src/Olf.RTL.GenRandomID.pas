7d4141594c1e4f70d4347f81c9f5a8b8dbb5ca24
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-04-18 15:19:23 +0200

    allow to change default Id's size

--- src/Olf.RTL.GenRandomID.pas
+++ src/Olf.RTL.GenRandomID.pas
@@ -8,36 +8,53 @@ type
     /// <summary>
     /// Generate a string composed by a random list of chars (0..9 -> numbers, 0..35 -> numbers + lowercased letters ('a'-'z'), 0 ..61 -> numbers and letters ('a'-'z' and 'A'-'Z')
     /// </summary>
-    class function getID(Const Base: Byte; Const Size: cardinal): string;
+    class function getID(Const Base: Byte; Const ASize: cardinal = 0): string;
     /// <summary>
     /// Generate a string with random numbers between '0' and '1'
     /// </summary>
-    class function getIDBase2(Const Size: cardinal = 10): string;
+    class function getIDBase2(Const Size: cardinal = 0): string;
     /// <summary>
     /// Generate a string with random numbers between '0' and '9'
     /// </summary>
-    class function getIDBase10(Const Size: cardinal = 10): string;
+    class function getIDBase10(Const Size: cardinal = 0): string;
     /// <summary>
     /// Generate a string with random numbers between '0' and '9' or letters between 'a' and 'z'
     /// </summary>
-    class function getIDBase36(Const Size: cardinal = 10): string;
+    class function getIDBase36(Const Size: cardinal = 0): string;
     /// <summary>
     /// Generate a string with random numbers between '0' and '9' or letters between 'a' and 'z' or 'A' and 'Z'
     /// </summary>
-    class function getIDBase62(Const Size: cardinal = 10): string;
+    class function getIDBase62(Const Size: cardinal = 0): string;
+    /// <summary>
+    /// Set the default size used by other methods of this class when no size is given when generating an ID.
+    /// </summary>
+    class procedure SetDefaultSize(Const Size: cardinal);
   end;
 
 implementation
 
-{ TOlfRandomIDGenerator }
+uses
+  System.SysUtils;
+
+var
+  DefaultSize: cardinal;
+
+  { TOlfRandomIDGenerator }
 
 class function TOlfRandomIDGenerator.getID(const Base: Byte;
-  const Size: cardinal): string;
+  const ASize: cardinal): string;
 var
   i, n: Byte;
+  LSize: cardinal;
 begin
   result := '';
-  for i := 1 to Size do
+
+  if ASize > 0 then
+    LSize := ASize
+  else
+    LSize := DefaultSize;
+
+  for i := 1 to LSize do
   begin
     n := random(Base);
     case n of
@@ -71,8 +88,17 @@ begin
   result := getID(62, Size);
 end;
 
+class procedure TOlfRandomIDGenerator.SetDefaultSize(const Size: cardinal);
+begin
+  if (Size < 1) then
+    raise exception.create('Size must be greater then 0.');
+
+  DefaultSize := Size;
+end;
+
 initialization
 
 randomize;
+DefaultSize := 10;
 
 end.
