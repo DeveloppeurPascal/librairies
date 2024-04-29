2ab9befd38c41fd481d4354d7e2ec45c6f3b66c4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-04-29 14:47:02 +0200

    added dump stream in this sample (#80)

--- samples/RTL.Streams/LoadAndSaveString/LoadAndSaveStringSample.dpr
+++ samples/RTL.Streams/LoadAndSaveString/LoadAndSaveStringSample.dpr
@@ -4,7 +4,8 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {Form1},
-  Olf.RTL.Streams in '..\..\..\src\Olf.RTL.Streams.pas';
+  Olf.RTL.Streams in '..\..\..\src\Olf.RTL.Streams.pas',
+  Olf.RTL.Maths.Conversions in '..\..\..\src\Olf.RTL.Maths.Conversions.pas';
 
 {$R *.res}
 
--- samples/RTL.Streams/LoadAndSaveString/fMain.pas
+++ samples/RTL.Streams/LoadAndSaveString/fMain.pas
@@ -3,7 +3,8 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
   FMX.ScrollBox, FMX.Memo, FMX.StdCtrls, FMX.Controls.Presentation, FMX.Edit;
 
@@ -34,21 +35,26 @@ uses
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  s:tmemorystream;
-  size:int64;
+  s: tmemorystream;
+  size: int64;
 begin
   if Edit1.text.isEmpty then
     exit;
 
-  s:=tmemorystream.create;
+  s := tmemorystream.create;
   try
-    SaveStringToStream(edit1.Text, s);
+    SaveStringToStream(Edit1.text, s);
     size := s.size;
     s.write(size, sizeof(size));
-    SaveStringToStream(edit1.Text,s);
+    SaveStringToStream(Edit1.text, s);
     //
     s.position := 0;
-    memo1.lines.LoadFromStream(s,TEncoding.UTF8);
+    Memo1.lines.LoadFromStream(s, TEncoding.UTF8);
+    //
+    Memo1.lines.add('----------');
+    Memo1.lines.add(DumpStream(s));
+    Memo1.lines.add('----------');
+    Memo1.lines.add(DumpStream(s, false));
   finally
     s.free;
   end;
@@ -56,27 +62,32 @@ end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 var
-  s:tmemorystream;
-  size:int64;
-  ch : string;
+  s: tmemorystream;
+  size: int64;
+  ch: string;
 begin
   if Edit1.text.isEmpty then
     exit;
 
-  s:=tmemorystream.create;
+  s := tmemorystream.create;
   try
-    SaveStringToStream(edit1.Text, s);
+    SaveStringToStream(Edit1.text, s);
     size := s.size;
     s.write(size, sizeof(size));
-    SaveStringToStream(edit1.Text,s);
+    SaveStringToStream(Edit1.text, s);
     //
-    memo1.lines.add('----------');
+    Memo1.lines.add('----------');
     //
     s.position := 0;
-    memo1.lines.add(LoadStringFromStream(s));
+    Memo1.lines.add(LoadStringFromStream(s));
     s.read(size, sizeof(size));
-    memo1.lines.add(size.tostring);
-    memo1.lines.add(LoadStringFromStream(s));
+    Memo1.lines.add(size.tostring);
+    Memo1.lines.add(LoadStringFromStream(s));
+    //
+    Memo1.lines.add('----------');
+    Memo1.lines.add(DumpStream(s));
+    Memo1.lines.add('----------');
+    Memo1.lines.add(DumpStream(s, false));
   finally
     s.free;
   end;
@@ -84,14 +95,14 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 var
-  i : integer;
+  i: integer;
   ch: string;
 begin
   randomize;
   ch := '';
   for i := 0 to 50 do
-    ch := ch + chr(ord('a')+random(26));
-  edit1.text := ch;
+    ch := ch + chr(ord('a') + random(26));
+  Edit1.text := ch;
 end;
 
 end.
