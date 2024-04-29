968c79dd8de8cd2f1a7a63a57b479c71f21bafd4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-04-29 15:14:47 +0200

    added sample use for Clear and Delete methods (#75)

--- samples/RTL.Params/StorageFolderName/fMain.pas
+++ samples/RTL.Params/StorageFolderName/fMain.pas
@@ -18,7 +18,8 @@ uses
   FMX.Memo,
   FMX.StdCtrls,
   FMX.Controls.Presentation,
-  FMX.Edit;
+  FMX.Edit,
+  FMX.Layouts;
 
 type
   TfrmMain = class(TForm)
@@ -27,8 +28,15 @@ type
     btnDefaultFolder: TButton;
     btnDefaultFolderV2: TButton;
     Memo1: TMemo;
+    GridPanelLayout1: TGridPanelLayout;
+    btnCreateParamFile: TButton;
+    btnDeleteParamFile: TButton;
+    btnClear: TButton;
     procedure btnDefaultFolderClick(Sender: TObject);
     procedure btnDefaultFolderV2Click(Sender: TObject);
+    procedure btnCreateParamFileClick(Sender: TObject);
+    procedure btnDeleteParamFileClick(Sender: TObject);
+    procedure btnClearClick(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -43,18 +51,53 @@ implementation
 {$R *.fmx}
 
 uses
+  System.IOUtils,
   Olf.RTL.Params;
+
+procedure TfrmMain.btnClearClick(Sender: TObject);
+begin
+  TParams.Clear;
+  TParams.save;
+  Memo1.lines.add(TParams.ToJSON);
+end;
+
+procedure TfrmMain.btnCreateParamFileClick(Sender: TObject);
+begin
+  TParams.setValue(random(500).tostring, random(500));
+  TParams.save;
+  if tfile.exists(TParams.getFilePath) then
+    Memo1.lines.add('params saved in ' + TParams.getFilePath)
+  else
+    Memo1.lines.add(TParams.getFilePath + ' doesn''t exist');
+  Memo1.lines.add(TParams.ToJSON);
+end;
 
 procedure TfrmMain.btnDefaultFolderClick(Sender: TObject);
 begin
-  tparams.setFilePath('');
-  Memo1.lines.add(tparams.getFilePath);
+  TParams.setFilePath('');
+  Memo1.lines.add(TParams.getFilePath);
 end;
 
 procedure TfrmMain.btnDefaultFolderV2Click(Sender: TObject);
 begin
-  tparams.InitDefaultFileNameV2(Edit1.Text, Edit2.Text);
-  Memo1.lines.add(tparams.getFilePath);
+  TParams.InitDefaultFileNameV2(Edit1.Text, Edit2.Text);
+  Memo1.lines.add(TParams.getFilePath);
+end;
+
+procedure TfrmMain.btnDeleteParamFileClick(Sender: TObject);
+begin
+  if tfile.exists(TParams.getFilePath) then
+  begin
+    Memo1.lines.add(TParams.getFilePath + ' exists');
+    TParams.Delete;
+    if tfile.exists(TParams.getFilePath) then
+      Memo1.lines.add('can''t delete ' + TParams.getFilePath)
+    else
+      Memo1.lines.add(TParams.getFilePath + ' deleted');
+  end
+  else
+    Memo1.lines.add(TParams.getFilePath + ' doesn''t exist');
+  Memo1.lines.add(TParams.ToJSON);
 end;
 
 end.
