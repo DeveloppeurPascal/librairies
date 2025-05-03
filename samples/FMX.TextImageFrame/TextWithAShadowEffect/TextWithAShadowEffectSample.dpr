9d5c81d68ceec688c6801d0f3e91d6fb430c6752
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-03 10:25:28 +0200

    added copyright and timestamp comments

--- samples/FMX.TextImageFrame/TextWithAShadowEffect/TextWithAShadowEffectSample.dpr
+++ samples/FMX.TextImageFrame/TextWithAShadowEffect/TextWithAShadowEffectSample.dpr
@@ -1,3 +1,46 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// My libraries for Delphi
+///
+/// Copyright 1990-2025 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// This repository contains functions, procedures and classes to use in
+/// Delphi projects (console, VCL, FireMonkey and others). It's my "everything reuseable things" toolbox.
+///
+/// The units to be used in your projects can be found in the "src" folder.
+/// Some features are explained on my blog or have been coded live on Twitch.
+///
+/// Examples of use in the form of VCL or FireMonkey projects are available in
+/// the "samples" subfolder.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://librairies.developpeur-pascal.fr
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/librairies
+///
+/// ***************************************************************************
+/// File last update : 2025-05-03T10:22:02.000+02:00
+/// Signature : c117d0c7c85b607a31cf534b3319380cae187e96
+/// ***************************************************************************
+/// </summary>
+
 program TextWithAShadowEffectSample;
 
 uses
