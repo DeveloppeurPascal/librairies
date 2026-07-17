71483d26cfe53fb7c91960ce222909660a3865d0
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2026-07-17 19:06:18 +0200

    added or updated copyright and timestamp header comments

--- samples/RTL.GenRandomID/RandomPassword/fMain.pas
+++ samples/RTL.GenRandomID/RandomPassword/fMain.pas
@@ -1,3 +1,50 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  My libraries for Delphi
+  Copyright (c) 1990-2026 Patrick PREMARTIN
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  ***************************************************************************
+
+  This repository contains functions, procedures and classes to use in
+  Delphi projects (console, VCL, FireMonkey and others). It's my "everything reuseable things" toolbox.
+
+  The units to be used in your projects can be found in the "src" folder.
+  Some features are explained on my blog or have been coded live on Twitch.
+
+  Examples of use in the form of VCL or FireMonkey projects are available in
+  the "samples" subfolder.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://librairies.developpeur-pascal.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/librairies
+
+  ***************************************************************************
+  File last update : 2026-07-17T19:02:58.000+02:00
+  Signature : 3d7ec22b7cf51562aeda47c8cc98ea96c5d002be
+  ***************************************************************************
+*)
+
 unit fMain;
 
 interface
--- src/Olf.RTL.GenRandomID.pas
+++ src/Olf.RTL.GenRandomID.pas
@@ -1,48 +1,49 @@
 ﻿(* C2PP
-***************************************************************************
+  ***************************************************************************
 
-My libraries for Delphi
-Copyright (c) 1990-2026 Patrick PREMARTIN
+  My libraries for Delphi
+  Copyright (c) 1990-2026 Patrick PREMARTIN
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-***************************************************************************
+  ***************************************************************************
 
-This repository contains functions, procedures and classes to use in
-Delphi projects (console, VCL, FireMonkey and others). It's my "everything reuseable things" toolbox.
+  This repository contains functions, procedures and classes to use in
+  Delphi projects (console, VCL, FireMonkey and others). It's my "everything reuseable things" toolbox.
 
-The units to be used in your projects can be found in the "src" folder.
-Some features are explained on my blog or have been coded live on Twitch.
+  The units to be used in your projects can be found in the "src" folder.
+  Some features are explained on my blog or have been coded live on Twitch.
 
-Examples of use in the form of VCL or FireMonkey projects are available in
-the "samples" subfolder.
+  Examples of use in the form of VCL or FireMonkey projects are available in
+  the "samples" subfolder.
 
-***************************************************************************
+  ***************************************************************************
 
-Author(s) :
-Patrick PREMARTIN
+  Author(s) :
+  Patrick PREMARTIN
 
-Site :
-https://librairies.developpeur-pascal.fr
+  Site :
+  https://librairies.developpeur-pascal.fr
 
-Project site :
-https://github.com/DeveloppeurPascal/librairies
+  Project site :
+  https://github.com/DeveloppeurPascal/librairies
 
-***************************************************************************
-File last update : 2026-03-30T16:35:19.662+02:00
-Signature : d484dd2b8e5efb11dced8c9cb726c939e4034420
-*************************************************************************** *)
+  ***************************************************************************
+  File last update : 2026-07-17T18:51:28.000+02:00
+  Signature : 7516fd85182f6da2d7e70d37337a56eed580767d
+  ***************************************************************************
+*)
 
 unit Olf.RTL.GenRandomID;
 
